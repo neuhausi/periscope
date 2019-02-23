69892c49980d17cea86fd982ab4d7ae301c113e5
cb4ds <connie@aggregate-genius.com>
2019-02-23 00:15:52 +0000

    logging link fix

--- R/downloadFile.R
+++ R/downloadFile.R
@@ -95,7 +95,7 @@ downloadFileButton <- function(id,
 #' @param output provided by \code{shiny::callModule}
 #' @param session provided by \code{shiny::callModule}
 #' \cr \cr
-#' @param logger \link[logging]{logging} logger to use
+#' @param logger \link[logging:logging-package]{logging} logger to use
 #' @param filenameroot the base text used for user-downloaded file - can be
 #' either a character string or a reactive expression that returns a character
 #' string
@@ -116,7 +116,7 @@ downloadFileButton <- function(id,
 #' @seealso \link[periscope]{downloadFile_ValidateTypes}
 #' @seealso \link[periscope]{downloadFile_AvailableTypes}
 #' @seealso \link[shiny]{callModule}
-#' @seealso \link[logging]{logging}
+#' @seealso \link[logging:logging-package]{logging}
 #'
 #' @export
 downloadFile <- function(input, output, session, logger,
